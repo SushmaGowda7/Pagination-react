411c95ea247a705a398f7f3efc693cb6e5bcaff5
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2022-12-30 13:55:18 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -30,23 +30,34 @@ function App() {
     <Fragment>
       {products.length > 0 && <div className="products">
         {products.slice(page * 10 - 10, page * 10).map((prod) => {
-          return <span className="products__single" key={prod.id}>
-            <img src={prod.thumbnail} alt={prod.title} /> {/* alt is imp */}
+          return (
+            <span className="products__single" key={prod.id}>
+            <img src={prod.thumbnail} alt={prod.title} />
             <span>
               {prod.title}
             </span>
           </span>
-        })}
+        )})}
       </div>}
 
       {products.length > 0 && <div className="pagination">
-        <span onClick={() => selectPageHandler(page - 1)} className={page > 1 ? "" : "pagination__disable"}>◀</span>
+        <span 
+          onClick={() => selectPageHandler(page - 1)} 
+          className={page > 1 ? "" : "pagination__disable"}>◀
+        </span>
 
         {[...Array(products.length / 10)].map((_, i) => {
-          return <span key={i} className={page === i + 1 ? "pagination__selected" : ""} onClick={() => selectPageHandler(i + 1)}>{i + 1}</span>
-        })}
+          return (
+            <span key={i} 
+              className={page === i + 1 ? "pagination__selected" : ""} 
+              onClick={() => selectPageHandler(i + 1)}>{i + 1}
+            </span>
+        )})}
 
-        <span onClick={() => selectPageHandler(page + 1)} className={page < products.length / 10 ? "" : "pagination__disable"}>▶</span>
+        <span 
+          onClick={() => selectPageHandler(page + 1)} 
+          className={page < products.length / 10 ? "" : "pagination__disable"}>▶
+        </span>
       </div>}
     </Fragment>
   );
